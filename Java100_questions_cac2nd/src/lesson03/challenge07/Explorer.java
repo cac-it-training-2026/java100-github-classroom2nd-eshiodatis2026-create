77b6f800ba74641418abd3d7172125ab3623c172
e-shioda <e-shioda@CD26307L.arksystems.co.jp>
2026-04-16 16:06:19 +0900

    0307

--- Java100_questions_cac2nd/src/lesson03/challenge07/Explorer.java
+++ Java100_questions_cac2nd/src/lesson03/challenge07/Explorer.java
@@ -83,9 +83,60 @@ public class Explorer {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
 		//ここにwhile文、if文を利用した処理を記述
+		while (i < 3) {
+			System.out.println("隊長:");
+			System.out.println("どの手を出して通り抜けますか\n（グー… 1 : チョキ… 2 : パー… 3）＞");
 
+			String handStr = br.readLine();
+			hand = Integer.parseInt(handStr);
+
+			alligator = (int) (Math.random() * 10 % 3) + 1;
+
+			//グー… 1 : チョキ… 2 : パー… 3
+
+			if (hand == 1) {//自分の手が1の時だけこの処理を実行する
+				if (alligator == 1 || alligator == 2) {
+					System.out.println("隊長：");
+					String kind = (alligator == 1) ? "グーワニ" : "チョキワニ";//条件（alligator == 1）の時、グーワニ、それ以外の時チョキワニ
+					System.out.println("相手は" + kind + "でした。");
+					System.out.println((i + 1) + "匹目を通り抜け成功！");
+					i++;
+				} else {
+					System.out.println("隊長：");
+					System.out.println("相手は:パーワニでした。");
+					break;
+				}
+			} else if (hand == 2) {
+				if (alligator == 2 || alligator == 3) {
+					System.out.println("隊長：");
+					String kind = (alligator == 2) ? "チョキワニ" : "パーワニ";
+					System.out.println("相手は" + kind + "でした。");
+					System.out.println((i + 1) + "匹目を通り抜け成功！");
+				} else {
+					System.out.println("隊長：");
+					System.out.println("相手はグーワニでした。");
+					break;
+				}
+			} else if (hand == 3) {
+				if (alligator == 3 || alligator == 1) {
+					System.out.println("隊長：");
+					String kind = (alligator == 3) ? "パーワニ" : "グーワニ";
+					System.out.println("相手は" + kind + "でした");
+					System.out.println((i + 1) + "匹目を通り抜け成功！");
+
+				} else {
+					System.out.println("隊長：");
+					System.out.println("相手はチョキワニでした。");
+					break;
+				}
+			} else {
+				System.out.println("隊長：");
+				System.out.println("そんな手はありませんよ。もう一度入れてください。");
+				i--;
+			}
+			i--;
+		}
 
 		if (i == 3) {
 			System.out.println("隊長：");
