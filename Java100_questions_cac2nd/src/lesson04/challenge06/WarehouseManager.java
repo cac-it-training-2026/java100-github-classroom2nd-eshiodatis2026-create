150f64818003584e3cce8ebcad8796ac76f68510
e-shioda <e-shioda@CD26307L.arksystems.co.jp>
2026-04-21 13:29:57 +0900

    0406

--- Java100_questions_cac2nd/src/lesson04/challenge06/WarehouseManager.java
+++ Java100_questions_cac2nd/src/lesson04/challenge06/WarehouseManager.java
@@ -39,9 +39,26 @@ public class WarehouseManager {
 
 		int[] ABKosanArray = new int[5];
 
-
 		//ここに重複チェックおよび値の代入処理を記述する
+		int inputNum = 0;
+		boolean loopFlag = false;
+		for (int i = 0; i < ABKosanArray.length; i++) {
+			do {
+				loopFlag = false;
+				inputNum = (int) (Math.random() * 10) % 5 + 1;
 
+				for (int j = 0; j < ABKosanArray.length; j++) {
+					if (ABKosanArray[j] == inputNum) {
+						loopFlag = true;
+						break;
+
+					}
+
+				}
+			} while (loopFlag);
+			ABKosanArray[i] = inputNum;
+
+		}
 
 		System.out.println("E主任：");
 		System.out.println("AB興産の荷物の入れ替えをお願いします。\n");
@@ -57,9 +74,21 @@ public class WarehouseManager {
 		}
 		System.out.println("\nです。\n");
 
-
 		//ここに値の入れ替え処理を記述する
+		for (int i = 0; i < ABKosanArray.length; i++) {
 
+			int value = ABKosanArray[i];
+
+			if (value == 1) {
+				ABKosanArray[i] = 3;
+			} else if (value == 2) {
+				ABKosanArray[i] = 4;
+			} else if (value == 3) {
+				ABKosanArray[i] = 1;
+			} else if (value == 4) {
+				ABKosanArray[i] = 2;
+			}
+		}
 
 		System.out.println("入れ替え後の状態は、");
 		for (int i = 0; i < ABKosanArray.length; i++) {
